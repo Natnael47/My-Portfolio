2333374c2a2eb28e9272afb0d11fe6dc7ed3eea9
Natnael47 <nati47m10@gmail.com>
2024-11-12 13:08:44 +0300

    fix on navbar

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import './Hero.css'
 
 const Hero = () => {
     return (
-        <div className='hero'>
+        <div id='home' className='hero'>
             <img src={profile_img} alt="" />
             <h1><span>I'm Alex Bennett,</span> Full stack Developer Base in Ethiopia</h1>
             <p>I'm both front and back end developer from Addis Abeba</p>
